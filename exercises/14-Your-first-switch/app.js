ec237a2e915f1b6ee665e9ea0f97d5aad61a1ad4
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-09 18:13:33 -0400

    ex14 done

--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,14 +1,20 @@
-function getColor(selection)
-{
-	switch(selection){
-		//Add more options here
-	    default :
-	    	return false;//returns false because the user picked an unavailable color
-	    break;               
-	}
+function getColor(selection) {
+    switch (selection) {
+        case 'red':
+            return true;
+        break;
+        case 'green':
+            return true;
+        break;
+        case 'blue':
+            return true;
+        break;
+        default:
+            return false;
+    }
 }
 
 var colorname = window.prompt('What color do you want?');
 var isAvailable = getColor(colorname);
-if(isAvailable) console.log('Good news! That color is available');
+if (isAvailable) console.log('Good news! That color is available');
 else console.log('We are sorry, that color is not available');
