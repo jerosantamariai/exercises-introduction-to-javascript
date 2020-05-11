c2010a22b02051f5b652f94f88509ea3959fdc29
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-11 15:40:53 -0400

    family doe done

--- exercises/25-Javascript-Objects/app.js
+++ exercises/25-Javascript-Objects/app.js
@@ -1,11 +1,13 @@
 var person = {
-    name: "John",                //String
+    name: "John",                    //String
     lastname: "Doe",
-    age: 35,                     //Number
+    age: 35,                         //Number
     gender: "male",
     lucky_numbers: [ 7, 11, 13, 17], //Array
-    significant_other: person2   //Object, yes the same variable/object defined after
+    significant_other: person2       //Object, yes the same variable/object defined after
 };
+
+person.lucky_numbers[3] = 33;
 
 var person2 = {
     name: "Jane",
@@ -16,23 +18,40 @@ var person2 = {
     significant_other: person
 };
 
+var person3 = {
+  name: "Jimmy",
+  lastname: "Doe",
+  age: 13,
+  gender: "male",
+  lucky_numbers: [1,2,3,4],
+  significant_other: null
+};
+//Little Jimmy's object
+
+
 var family = {
     lastname: "Doe",
-    members: [person, person2]   //Array of objects, don't forget to add Jimmy
+    members: [person, person2]       //Array of objects, don't forget to add Jimmy
 };
 
+family.members.push(person3)
 
 function addAllFamilyLuckyNumbers(anArray){
   var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
-
-  //To-Do: loop and add; consider nested loops
+ 
+  //To-Do: loop and add
   //Hint: use the anArray variable to get all of the lucky numbers
   
+
+  for(var k=0;k<family.members.length;k++){
+    
+    for(var l=0;l<family.members[k].lucky_numbers.length;l++){
+      sumOfAllLuckyNumbers+=family.members[k].lucky_numbers[l];
+    }
+    
+  }
+ 
   return sumOfAllLuckyNumbers;
 }
 
-//Enter all your code here:
-
-
-//Do not make changes below:
-console.log(addAllFamilyLuckyNumbers(family.members)); 
+console.log(addAllFamilyLuckyNumbers(family.members)); //Step 3
