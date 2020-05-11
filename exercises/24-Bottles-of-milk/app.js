9ee143a39fb0611ee00efa641f0eb5a39eb61b10
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-11 15:12:56 -0400

    bottles done

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,1 +1,25 @@
-// Your code here:
+function song() {
+    for (i=99; i>=0; i--){
+      var nums = i;
+      switch (nums) {
+    case 2:
+      text = i + " bottles of milk on the wall, " + i + " bottles of milk.\nTake one down and pass it around, " + (i-1) + " bottle of milk on the wall.\n\r";
+      break;
+    case 1:
+      text = i + " bottle of milk on the wall, " + i + " bottle of milk.\nTake one down and pass it around, no more bottles of milk on the wall.\n\r";
+      break;
+    case 0:
+      text = "No more bottles of milk on the wall, no more bottles of milk.\nGo to the store and buy some more, 99 bottles of milk on the wall.\n\r";
+      break;
+    default:
+      text = i + " bottles of milk on the wall, " + i + " bottles of milk.\nTake one down and pass it around, " + (i-1) + " bottles of milk on the wall.\n\r";
+  }
+      
+      
+      console.log(text);
+    }
+  }
+  
+  song();
+  
+  
