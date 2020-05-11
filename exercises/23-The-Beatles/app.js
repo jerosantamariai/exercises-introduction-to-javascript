8a50276251765aa5924ee79593c5403cd7b01d8c
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-11 14:54:23 -0400

    let it be

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,5 +1,22 @@
-
-
-//Your code above ^^^
+//Your code here!!
+function sing()
+{
+	var lyrics = 'let it be';
+	for(var i = 1; i<=9; i++)
+	{
+		lyrics = lyrics + ', let it be';
+		if(i==3)
+		{
+			lyrics = lyrics + ', words of wisdom';
+		}
+		if(i==8)
+		{
+			lyrics = lyrics + ', there will be an answer';
+			
+		}
+	}
+	return lyrics;
+	
+}
 
 console.log(sing());
